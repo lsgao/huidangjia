72aee873575ec0ebd0f679ed6fb734f9ec83eb09
oizhaolei <oizhaolei@gmail.com>
2018-04-11 10:05:05 +0800

    微信支付回调成功

--- mobile/include/modules/payment/wx_new_jspay.php
+++ mobile/include/modules/payment/wx_new_jspay.php
@@ -66,7 +66,7 @@ class wx_new_jspay
             define("WXKEY", $payment['key']);
             define("WXAPPSECRET", $payment['appsecret']);
             define("WXCURL_TIMEOUT", 30);
-            define('WXNOTIFY_URL',$root_url.'wx_native_callback.php');
+            define('WXNOTIFY_URL',$root_url.'mobile/wx_jsapi_callback.php');
             define('WXSSLCERT_PATH',dirname(__FILE__).'/WxPayPubHelper/cacert/apiclient_cert.pem');
             define('WXSSLKEY_PATH',dirname(__FILE__).'/WxPayPubHelper/cacert/apiclient_key.pem');
         }
@@ -162,54 +162,44 @@ class wx_new_jspay
         }
     }
 
-    function respond()
-    {
+    function respond() {
         $payment  = get_payment('wx_new_jspay');
 
         $notify = new Notify_pub();
         $xml = $GLOBALS['HTTP_RAW_POST_DATA'];
         $notify->saveData($xml);
-        if($payment['logs'])
-        {
+        if($payment['logs']) {
             $this->log(ROOT_PATH.'/data/wx_new_log.txt',"传递过来的XML\r\n".var_export($xml,true));
         }
-        if($notify->checkSign() == TRUE)
-        {
+        if($notify->checkSign() == TRUE) {
             if ($notify->data["return_code"] == "FAIL") {
                 //此处应该更新一下订单状态，商户自行增删操作
                 if($payment['logs']){
                     $this->log(ROOT_PATH.'/data/wx_new_log.txt',"return_code失败\r\n");
                 }
-            }
-            elseif($notify->data["result_code"] == "FAIL"){
+            } else if($notify->data["result_code"] == "FAIL") {
                 //此处应该更新一下订单状态，商户自行增删操作
                 if($payment['logs']){
                     $this->log(ROOT_PATH.'/data/wx_new_log.txt',"result_code失败\r\n");
                 }
-            }
-            else{
+            } else {
                 //此处应该更新一下订单状态，商户自行增删操作
-                if($payment['logs']){
+                if($payment['logs']) {
                     $this->log(ROOT_PATH.'/data/wx_new_log.txt',"支付成功\r\n");
                 }
                 $total_fee = $notify->data["total_fee"];
                 $log_id = $notify->data["attach"];
                 $sql = 'SELECT order_amount FROM ' . $GLOBALS['ecs']->table('pay_log') ." WHERE log_id = '$log_id'";
                 $amount = $GLOBALS['db']->getOne($sql);
-                
-                if($payment['logs'])
-                {
+
+                if($payment['logs']) {
                     $this->log(ROOT_PATH.'/data/wx_new_log.txt','订单金额'.$amount."\r\n");
                 }
-                
-                if(intval($amount*100) != $total_fee)
-                {
-                    
-                    if($payment['logs'])
-                    {   
+
+                if(intval($amount*100) != $total_fee) {
+                    if($payment['logs']) {
                         $this->log(ROOT_PATH.'/data/wx_new_log.txt','订单金额不符'."\r\n");
                     }
-                    
                     echo 'fail';
                     return;
                 }
@@ -218,22 +208,18 @@ class wx_new_jspay
                 return true;
             }
 
-        }
-        else
-        {
+        } else {
             $this->log(ROOT_PATH.'/data/wx_new_log.txt',"签名失败\r\n");
         }
         return false;
-       
     }
-    
-    function log($file,$txt)
-    {
-       $fp =  fopen($file,'ab+');
-       fwrite($fp,'-----------'.local_date('Y-m-d H:i:s').'-----------------');
-       fwrite($fp,$txt);
-       fwrite($fp,"\r\n\r\n\r\n");
-       fclose($fp);
+
+    function log($file,$txt) {
+        $fp =  fopen($file,'ab+');
+        fwrite($fp,'-----------'.local_date('Y-m-d H:i:s').'-----------------');
+        fwrite($fp,$txt);
+        fwrite($fp,"\r\n\r\n\r\n");
+        fclose($fp);
     }
     
 }
---
+++ mobile/wx_jsapi_callback.php
@@ -0,0 +1,22 @@
+<?php
+define('IN_ECS', true);
+define('IN_ECTOUCH', true);
+require(dirname(__FILE__) . '/include/init.php');
+require(dirname(__FILE__) . '/include/lib_payment.php');
+require_once(dirname(__FILE__) .'/include/modules/payment/wx_new_jspay.php');
+
+write_log(dirname(__FILE__).'/data/wx_new_log.txt', dirname(__FILE__) ."/include/modules/payment/wx_new_jspay.php\r\n");
+$payment = new wx_new_jspay();
+$payment->respond();
+exit;
+
+    function write_log($file,$txt)
+    {
+       $fp =  fopen($file,'ab+');
+       fwrite($fp,'-----------'.date('Y-m-d H:i:s').'-----------------');
+       fwrite($fp,$txt);
+       fwrite($fp,"\r\n\r\n\r\n");
+       fclose($fp);
+    }
+
+?>
