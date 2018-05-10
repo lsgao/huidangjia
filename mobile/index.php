ba7a24a2926d00aa52f7a5476d53842f77df350f
oizhaolei <oizhaolei@gmail.com>
2018-05-10 15:42:59 +0800

    首页增加明日预告

--- mobile/index.php
+++ mobile/index.php
@@ -69,6 +69,7 @@ if (!$smarty->is_cached('index.dwt', $cache_id))
     $smarty->assign('group_buy_goods', index_get_group_buy());      // 团购商品
     $smarty->assign('auction_list',    index_get_auction());        // 拍卖活动
     $smarty->assign('shop_notice',     $_CFG['shop_notice']);       // 商店公告
+    $smarty->assign('tomorrow_goods',      get_category_recommend_goods("promotion_goods", "g.cat_id IN ('227')"));
 
     /* 首页主广告设置 */
     $smarty->assign('index_ad',     $_CFG['index_ad']);
